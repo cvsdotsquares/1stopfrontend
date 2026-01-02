e4efed3ad424d41b0c549c2a8d6fcdb9f3606377
cvsdotsquares <chandraveer.singh@dotsquares.com>
2026-01-02 17:12:08 +0530

    Add svg adjustments according to next

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -116,7 +116,7 @@ export default function Hero({ data }: { data: HeroData }) {
                 />
 
                 <span className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2 text-indigo-600">
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search h-5 w-5" aria-hidden="true"><path d="m21 21-4.34-4.34"></path><circle cx="11" cy="11" r="8"></circle></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search h-5 w-5" aria-hidden="true"><path d="m21 21-4.34-4.34"></path><circle cx="11" cy="11" r="8"></circle></svg>
                 </span>
               </div>
             </div>
